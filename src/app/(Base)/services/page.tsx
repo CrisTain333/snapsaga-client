b3c1f09e3fe4a1af71ca6f3ed5f4889163f7ceea
Sukanta Das <arnobh84@gmail.com>
2023-10-12 11:20:34 +0600

    service page desgin completed

--- src/app/(Base)/services/page.tsx
+++ src/app/(Base)/services/page.tsx
@@ -1,7 +1,31 @@
-import React from "react";
+import FilterSection from "@/components/filter/FilterSection/FilterSection";
+import ServiceCard from "@/components/ui/ServiceCard/ServiceCard";
+import React, { useState } from "react";
 
 const page = () => {
-  return <div></div>;
+  const arr = [1, 2, 3, 4, 5, 6, 7, 8, 9];
+  return (
+    <div>
+      <div className="w-[95%] mx-auto">
+        <div className="grid grid-cols-12 gap-5">
+          <div className="col-span-3">
+            <div className="sticky top-20">
+              <FilterSection />
+            </div>
+          </div>
+          <div className="col-span-9">
+            <div className="grid grid-cols-12 gap-5">
+              {arr.map((t) => (
+                <div className="col-span-4">
+                  <ServiceCard />
+                </div>
+              ))}
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
 };
 
 export default page;
