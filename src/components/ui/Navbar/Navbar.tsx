2b5e548a71b44d30456f3106aa40cadbec86e53d
Sukanta Das <arnobh84@gmail.com>
2023-10-13 12:03:55 +0600

    some change in navbar

--- src/components/ui/Navbar/Navbar.tsx
+++ src/components/ui/Navbar/Navbar.tsx
@@ -14,6 +14,7 @@ import {
 
 import { Button } from "@/components/ui/button";
 import { logout } from "@/redux/feature/user/userSlice";
+import { LogOut } from "lucide-react";
 
 export default () => {
   const [state, setState] = useState(false);
@@ -154,18 +155,7 @@ export default () => {
                   onClick={() => dispatch(logout())}
                 >
                   Logout
-                  <svg
-                    xmlns="http://www.w3.org/2000/svg"
-                    viewBox="0 0 20 20"
-                    fill="currentColor"
-                    className="w-5 h-5"
-                  >
-                    <path
-                      fillRule="evenodd"
-                      d="M7.21 14.77a.75.75 0 01.02-1.06L11.168 10 7.23 6.29a.75.75 0 111.04-1.08l4.5 4.25a.75.75 0 010 1.08l-4.5 4.25a.75.75 0 01-1.06-.02z"
-                      clipRule="evenodd"
-                    />
-                  </svg>
+                  <LogOut />
                 </Button>
               </>
             )}
