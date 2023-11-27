9c211bc29b56ac0350c0950ef217138e73c462aa
Sukanta Das <arnobh84@gmail.com>
2023-11-27 11:29:47 +0600

    new ui added for the booking page

--- src/app/(Base)/booking/page.tsx
+++ src/app/(Base)/booking/page.tsx
@@ -100,7 +100,7 @@ const page = () => {
 
   return (
     <div className="my-10">
-      <div className="mt-5">
+      {/* <div className="mt-5">
         <h2 className="text-center text-3xl font-semibold">
           Booking Of {bookingItem?.title}
         </h2>
@@ -236,7 +236,140 @@ const page = () => {
             </form>
           </div>
         </div>
+      </div> */}
+
+      <div className="">
+        <div className="relative mx-auto  mb-20 max-w-screen-lg overflow-hidden rounded-t-xl bg-emerald-400/60 py-32 text-center shadow-xl shadow-gray-300">
+          <h1 className="mt-2 px-8 text-3xl font-bold text-white md:text-5xl">
+            Book an Service
+          </h1>
+          <p className="mt-6 text-lg text-white">
+            Get a Photo shoot with our experienced
+            photographer
+          </p>
+          <img
+            className="absolute top-0 left-0 -z-10 h-full w-full object-cover"
+            src="https://images.unsplash.com/photo-1504672281656-e4981d70414b?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1170&q=80"
+            alt=""
+          />
+        </div>
+
+        <div className="mx-auto grid max-w-screen-lg px-6 pb-20">
+          <div className="">
+            <p className="font-serif text-xl font-bold text-blue-900">
+              Select a service
+            </p>
+            <div className="mt-4 grid max-w-3xl gap-x-4 gap-y-3 sm:grid-cols-2 md:grid-cols-3">
+              <div className="relative">
+                <input
+                  className="peer hidden"
+                  id="radio_1"
+                  type="radio"
+                  name="radio"
+                  checked
+                />
+                <span className="absolute right-4 top-1/2 box-content block h-3 w-3 -translate-y-1/2 rounded-full border-8 border-gray-300 bg-white peer-checked:border-emerald-400"></span>
+                <label
+                  className="flex h-full cursor-pointer flex-col rounded-lg p-4 shadow-lg shadow-slate-100 peer-checked:bg-emerald-600 peer-checked:text-white"
+                  // for="radio_1"
+                >
+                  <span className="mt-2- font-medium">
+                    Financial Planning
+                  </span>
+                  <span className="text-xs uppercase">
+                    1 Hour
+                  </span>
+                </label>
+              </div>
+              <div className="relative">
+                <input
+                  className="peer hidden"
+                  id="radio_2"
+                  type="radio"
+                  name="radio"
+                />
+                <span className="absolute right-4 top-1/2 box-content block h-3 w-3 -translate-y-1/2 rounded-full border-8 border-gray-300 bg-white peer-checked:border-emerald-400"></span>
+
+                <label
+                  className="flex h-full cursor-pointer flex-col rounded-lg p-4 shadow-lg shadow-slate-100 peer-checked:bg-emerald-600 peer-checked:text-white"
+                  // for="radio_2"
+                >
+                  <span className="mt-2 font-medium">
+                    Retirement Planning
+                  </span>
+                  <span className="text-xs uppercase">
+                    1 Hour
+                  </span>
+                </label>
+              </div>
+              <div className="relative">
+                <input
+                  className="peer hidden"
+                  id="radio_3"
+                  type="radio"
+                  name="radio"
+                />
+                <span className="absolute right-4 top-1/2 box-content block h-3 w-3 -translate-y-1/2 rounded-full border-8 border-gray-300 bg-white peer-checked:border-emerald-400"></span>
+
+                <label
+                  className="flex h-full cursor-pointer flex-col rounded-lg p-4 shadow-lg shadow-slate-100 peer-checked:bg-emerald-600 peer-checked:text-white"
+                  // for="radio_3"
+                >
+                  <span className="mt-2 font-medium">
+                    Investment Advice
+                  </span>
+                  <span className="text-xs uppercase">
+                    1 Hour
+                  </span>
+                </label>
+              </div>
+            </div>
+          </div>
+
+          <div className="">
+            <p className="mt-8 font-serif text-xl font-bold text-blue-900">
+              Select a date
+            </p>
+          </div>
+
+          <div className="">
+            <p className="mt-8 font-serif text-xl font-bold text-blue-900">
+              Select a time
+            </p>
+            <div className="mt-4 grid grid-cols-4 gap-2 lg:max-w-xl">
+              <button className="rounded-lg bg-emerald-100 px-4 py-2 font-medium text-emerald-900 active:scale-95">
+                12:00
+              </button>
+              <button className="rounded-lg bg-emerald-100 px-4 py-2 font-medium text-emerald-900 active:scale-95">
+                14:00
+              </button>
+              <button className="rounded-lg bg-emerald-700 px-4 py-2 font-medium text-white active:scale-95">
+                09:00
+              </button>
+              <button className="rounded-lg bg-emerald-100 px-4 py-2 font-medium text-emerald-900 active:scale-95">
+                12:00
+              </button>
+              <button className="rounded-lg bg-emerald-100 px-4 py-2 font-medium text-emerald-900 active:scale-95">
+                15:00
+              </button>
+              <button className="rounded-lg bg-emerald-100 px-4 py-2 font-medium text-emerald-900 active:scale-95">
+                12:00
+              </button>
+              <button className="rounded-lg bg-emerald-100 px-4 py-2 font-medium text-emerald-900 active:scale-95">
+                14:00
+              </button>
+              <button className="rounded-lg bg-emerald-100 px-4 py-2 font-medium text-emerald-900 active:scale-95">
+                12:00
+              </button>
+            </div>
+          </div>
+
+          <button className="mt-8 w-56 rounded-full border-8 border-emerald-500 bg-emerald-600 px-10 py-4 text-lg font-bold text-white transition hover:translate-y-1">
+            Book Now
+          </button>
+        </div>
       </div>
+      <script src="https://unpkg.com/flowbite@1.5.2/dist/datepicker.js"></script>
     </div>
   );
 };
